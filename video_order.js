c64b23a8f859672d1039e3daf2d8bba80fa37cfe
zhangxc <zxc@zxc-Mac-mini.local>
2026-07-24 18:41:18 +0800

    merged video

--- video_order.js
+++ video_order.js
@@ -5,5 +5,26 @@ const VIDEO_ORDER = {
     "ours",
     "tcdiffstar",
     "tcdiffpp"
+  ],
+  "01": [
+    "tcdiffpp",
+    "edge",
+    "ours",
+    "tcdiffstar",
+    "tcdiff"
+  ],
+  "02": [
+    "tcdiffpp",
+    "tcdiff",
+    "edge",
+    "tcdiffstar",
+    "ours"
+  ],
+  "04": [
+    "tcdiffpp",
+    "tcdiffstar",
+    "tcdiff",
+    "edge",
+    "ours"
   ]
 };
